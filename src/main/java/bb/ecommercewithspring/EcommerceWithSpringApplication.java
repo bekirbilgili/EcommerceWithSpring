e5e3467be20a0a092f8429810c4e6889d12a3e10
Bekir Bilgili <mbekirbilgili@gmail.com>
2024-05-12 18:05:20 +0300

    initial start

---
+++ src/main/java/bb/ecommercewithspring/EcommerceWithSpringApplication.java
@@ -0,0 +1,13 @@
+package bb.ecommercewithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bb/ecommercewithspring/EcommerceWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package bb.ecommercewithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
